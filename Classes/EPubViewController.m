193cad6fa818f0397b1afa535d2cdf80890c3a4a
Daniel Weck <daniel.weck@gmail.com>
2013-10-19 18:20:59 +0100

    automatic breakpoint

--- Classes/EPubViewController.m
+++ Classes/EPubViewController.m
@@ -20,6 +20,45 @@
 #import "RDPackageResource.h"
 #import "RDSpineItem.h"
 
+#include <assert.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/sysctl.h>
+
+//https://developer.apple.com/library/mac/qa/qa1361/_index.html
+static bool AmIBeingDebugged(void)
+// Returns true if the current process is being debugged (either
+// running under the debugger or has a debugger attached post facto).
+{
+    int                 junk;
+    int                 mib[4];
+    struct kinfo_proc   info;
+    size_t              size;
+
+    // Initialize the flags so that, if sysctl fails for some bizarre
+    // reason, we get a predictable result.
+
+    info.kp_proc.p_flag = 0;
+
+    // Initialize mib, which tells sysctl the info we want, in this case
+    // we're looking for information about a specific process ID.
+
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    // Call sysctl.
+
+    size = sizeof(info);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    assert(junk == 0);
+
+    // We're being debugged if the P_TRACED flag is set.
+
+    return ( (info.kp_proc.p_flag & P_TRACED) != 0 );
+}
 
 @interface EPubViewController ()
 
@@ -593,13 +632,18 @@
 				NSString *arg = [[[NSString alloc] initWithData:data
 					encoding:NSUTF8StringEncoding] autorelease];
 
-				[m_webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:
+                NSString* jsCode = @"ReadiumSDK.reader.openBook(%@)";
 #ifdef DEBUG
-                        @"setTimeout(function(){ReadiumSDK.reader.openBook(%@);}, 1000)",
-#else
-                        @"ReadiumSDK.reader.openBook(%@)",
+if (AmIBeingDebugged())
+{
+    // OPEN THE SAFARI REMOTE DEBUGGER HERE, THEN RESUME EXECUTION! :)
+    kill (getpid(), SIGSTOP);
+
+    jsCode = @"setTimeout(function(){ReadiumSDK.reader.openBook(%@);}, 1000)";
+}
 #endif
-                    arg]];
+
+                [m_webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat: jsCode, arg]];
 			}
 		}
 		else if ([url hasPrefix:s]) {
